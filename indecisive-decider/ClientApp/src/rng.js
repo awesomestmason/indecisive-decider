cb798c7ed15a923c3c1f62240c4fc83a3af7e96d
Evoluze <richardrep@hotmail.com>
2021-10-25 20:18:00 -0700

    rng code added in

---
+++ indecisive-decider/ClientApp/src/rng.js
@@ -0,0 +1,34 @@
+function createList(wordlist)
+{
+    const userList = wordlist.split(", ");
+    var counter = userList.length;
+    for(int i = 0; i < userList.length; i++)
+    {
+        userList[i].trim();
+    }
+    var rand = Math.floor(Math.random() * (counter + 1);
+    return userList[rand];
+}
+
+//just a getting random function from a min and max value
+function getRandomNum(min, max)
+{
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+//function for the presets
+//list should already have the items in an array/list order
+//presets assumed in array functionality with objects.
+//take in presets
+function getPreset(items)
+{
+    var counter = items.length;
+    var rand = Math.floor(Math.random() * (counter + 1);
+    return items[rand].value;
+}
+
+/*function getRandomFromList()
+{
+    var x = Math.floor(Math.random() * (userList.length + 1));
+    return userList[x];
+}*/
