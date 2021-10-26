bf74a88cffe50004b0959f161172cb0801e0c7fd
Qiance Yu <dakeyu2011@gmail.com>
2021-10-25 23:59:20 -0700

    Remove commented out code

--- indecisive-decider/Startup.cs
+++ indecisive-decider/Startup.cs
@@ -122,21 +122,6 @@ namespace indecisive_decider
                     spa.UseReactDevelopmentServer(npmScript: "start");
                 }
             });
-
-            // // Enable middleware to serve generated Swagger as a JSON endpoint.
-            // app.UseSwagger();
-
-            // // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.)
-            // app.UseSwaggerUI(c =>
-            // {
-            //     c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-            // });
-
-            // app.UseRouting();
-            // app.UseEndpoints(endpoints =>
-            // {
-            //     endpoints.MapControllers();
-            // });
         }
     }
 }
